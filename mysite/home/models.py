c12c9892663c74a198f0a2252731f16aae5bd596
Oleg Khalidov <brooth@gmail.com>
2016-05-03 00:25:22 +0400

    sitemap

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 import os
 
+from django.utils import timezone
 from django.db import models
 from django.conf import settings
+from django.contrib.sitemaps import Sitemap
 
 from markdown import markdown
 
@@ -12,6 +14,7 @@ class MarkdownPage(models.Model):
     path = models.CharField(max_length=140, unique=True, blank=True)
     md_filename = models.CharField(max_length=100)
     html_content = models.TextField(editable=False, blank=True, null=True)
+    modified_ts = models.DateTimeField(default=timezone.now)
 
     def save(self, *args, **kwargs):
         filename = settings.MARKDOWN_FILES_URL + self.md_filename
@@ -29,3 +32,28 @@ class MarkdownPage(models.Model):
 
     def __str__(self):
         return self.title + ' ~ (' + self.path + ') ~ (' + self.md_filename + ')'
+
+
+class MarkdownPageSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.5
+
+    def items(self):
+        return MarkdownPage.objects.all()
+
+    def lastmod(self, obj):
+        return obj.modified_ts
+
+    def location(self, item):
+        return '/' + item.path
+
+
+class StaticSitemap(Sitemap):
+    priority = 0.6
+    changefreq = 'monthly'
+
+    def items(self):
+        return ['/donate.html']
+
+    def location(self, item):
+        return item
--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,10 +2,19 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from django.contrib.sitemaps.views import sitemap
+from home.models import MarkdownPageSitemap, StaticSitemap
+
 from . import views
 
 
+sitemaps = {
+    'posts': MarkdownPageSitemap,
+    'static': StaticSitemap,
+}
+
 urlpatterns = [
-    url(r'^donate.html', TemplateView.as_view(template_name='home/donate.html')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^donate\.html$', TemplateView.as_view(template_name='home/donate.html')),
     url(r'^(?P<path>.*)$', views.markdown_page),
 ]
